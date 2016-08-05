8ffdc19ace5581102b900f218785a1a851601343
roman <romka55_7@mail.ru>
2016-08-06 00:42:54 +0600

    create server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,24 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.AllRequestsServlet;
+
+import javax.servlet.http.HttpServlet;
+
 /**
  * Created by roman on 05.08.16.
  */
 public class Main {
-    public static void main (String[] args){
-        System.out.println("Test");
+    public static void main (String[] args) throws Exception {
+        HttpServlet servlet = new AllRequestsServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(servlet),"/*");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        server.join();
     }
 }
---
+++ src/main/java/servlets/AllRequestsServlet.java
@@ -0,0 +1,22 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by roman on 06.08.16.
+ */
+public class AllRequestsServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
